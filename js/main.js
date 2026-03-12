3cf122e2ca844180e66ddbbd532822f059e8ef9b
caromaly <carocaromaly@gmail.com>
2026-03-12 22:35:32 +0300

    blya

--- js/main.js
+++ js/main.js
@@ -120,4 +120,120 @@ letters.forEach(letter => {
   });
 });
 
+
+
+
+
+
+const container = document.querySelector('.div1');
+const svg = document.querySelector('.links');
+
+const main = document.querySelector('.main');
+const buttons = document.querySelectorAll('.but');
+
+const lines = [];
+
+buttons.forEach(() => {
+  const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+  line.setAttribute("stroke", "black");
+  line.setAttribute("stroke-width", "3");
+  svg.appendChild(line);
+  lines.push(line);
+});
+
+function center(el) {
+  const rect = el.getBoundingClientRect();
+  const parent = container.getBoundingClientRect();
+
+  return {
+    x: rect.left - parent.left + rect.width / 2,
+    y: rect.top - parent.top + rect.height / 2
+  };
+}
+
+function update() {
+
+  const mainPos = center(main);
+
+  buttons.forEach((btn, i) => {
+    const btnPos = center(btn);
+
+    lines[i].setAttribute("x1", mainPos.x);
+    lines[i].setAttribute("y1", mainPos.y);
+    lines[i].setAttribute("x2", btnPos.x);
+    lines[i].setAttribute("y2", btnPos.y);
+  });
+
+  requestAnimationFrame(update);
+}
+
+update();
+
+function shortenLine(x1, y1, x2, y2, cut) {
+
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+
+  const length = Math.sqrt(dx * dx + dy * dy);
+
+  const offsetX = (dx / length) * cut;
+  const offsetY = (dy / length) * cut;
+
+  return {
+    x1: x1 + offsetX,
+    y1: y1 + offsetY,
+    x2: x2 - offsetX,
+    y2: y2 - offsetY
+  };
+}
+
+function update() {
+
+  const mainPos = center(main);
+
+  buttons.forEach((btn, i) => {
+
+    const btnPos = center(btn);
+
+    const linePos = shortenLine(
+      mainPos.x,
+      mainPos.y,
+      btnPos.x,
+      btnPos.y,
+      100 // сколько обрезать
+    );
+
+    lines[i].setAttribute("x1", linePos.x1);
+    lines[i].setAttribute("y1", linePos.y1);
+    lines[i].setAttribute("x2", linePos.x2);
+    lines[i].setAttribute("y2", linePos.y2);
+  });
+
+  requestAnimationFrame(update);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
